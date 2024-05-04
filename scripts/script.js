1d5a5975b5dabefab169caa9a2305b597495b8f8
jaydengreen23 <157416940+jaydengreen23@users.noreply.github.com>
2024-05-04 18:40:01 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    $('#generateBtn').click(function () {
+        var inputText = $('#textInput').val();
+        var outputList = $('#outputList');
+        outputList.empty();
+
+        for (var i = 0; i < inputText.length; i++) {
+            var letter = inputText[i].toUpperCase();
+            var phonetic = getPhonetic(letter);
+            outputList.append('<li class="list-group-item">' + letter + ' - ' + phonetic + '</li>');
+        }
+    });
+
+    $('#clearBtn').click(function () {
+        $('#outputList').empty();
+    });
+
+    function getPhonetic(letter) {
+        switch (letter) {
+            case 'A':
+                return 'Alpha';
+            case 'B':
+                return 'Bravo';
+            case 'C':
+                return 'Charlie';
+            case 'D':
+                return 'Delta';
+            case 'E':
+                return 'Echo';
+            case 'F':
+                return 'Foxtrot';
+            case 'G':
+                return 'Golf';
+            case 'H':
+                return 'Hotel';
+            case 'I':
+                return 'India';
+            case 'J':
+                return 'Juliett';
+            case 'K':
+                return 'Kilo';
+            case 'L':
+                return 'Lima';
+            case 'M':
+                return 'Mike';
+            case 'N':
+                return 'November';
+            case 'O':
+                return 'Oscar';
+            case 'P':
+                return 'Papa';
+            case 'Q':
+                return 'Quebec';
+            case 'R':
+                return 'Romeo';
+            case 'S':
+                  return 'Sierra';
+            case 'T':
+                return 'Tango';
+            case 'U':
+                return 'Uniform';
+            case 'V':
+                return 'Victor';
+            case 'W':
+                return 'Whiskey';
+            case 'X':
+                return 'X-ray';
+            case 'Y':
+                return 'Yankee';
+            case 'Z':
+                return 'Zulu';
+            default:
+                return 'Unknown';
+        }
+    }
+});
